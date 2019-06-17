5d31a19508baf15493b9e3a7f4e38924e7448377
benjaminchazelle <bchazelle.dev@outlook.fr>
2019-06-17 16:58:32 +0200

    Add files via upload

---
+++ pfe/media.js
@@ -0,0 +1,13 @@
+medias = {
+	"10" : "medias/flight_preprocessing_first.mp4",
+
+	"11" : "medias/flight_preprocessing_second.mp4",
+
+	"18" : "medias/windowing.mp4",
+
+	"22" : "medias/impacts3.mp4",
+
+	"27" : "medias/centrocourse.mp4"
+
+
+}
